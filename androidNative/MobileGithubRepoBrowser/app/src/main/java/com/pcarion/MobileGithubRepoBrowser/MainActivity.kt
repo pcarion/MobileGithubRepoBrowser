4e4e91bd19e73ebae14d526c86a8979cec3e2144
Pierre Carion <pierre@rabb.it>
2019-06-09 10:10:29 -0700

    wire home page

--- androidNative/MobileGithubRepoBrowser/app/src/main/java/com/pcarion/MobileGithubRepoBrowser/MainActivity.kt
+++ androidNative/MobileGithubRepoBrowser/app/src/main/java/com/pcarion/MobileGithubRepoBrowser/MainActivity.kt
@@ -5,9 +5,22 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 //provides view binding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+//import android.R
+//import androidx.core.view.ViewCompat.setAlpha
+//import androidx.core.view.ViewCompat.animate
+//import android.widget.AdapterView
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +31,29 @@ class MainActivity : AppCompatActivity() {
 
         actionBar?.title = "Github User"
         supportActionBar?.title = "Github User"
+
+        val listItems = arrayOf(
+            "intuit",
+            "facebook",
+            "pcarion"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listSuggestions.setAdapter(adapter)
+
+        listSuggestions.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override  fun onItemClick(
+                parent: AdapterView<*>, view: View,
+                position: Int, id: Long
+            ) {
+                val item = parent.getItemAtPosition(position) as String
+                inputUserName?.setText(item)
+            }
+        }
+
+        selectButton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+        }
     }
 }
